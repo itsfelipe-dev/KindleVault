88cba46439438c2ae35b8ea891909c447c7ae5fc
Andres <aforjuela11@misena.edu.co>
2025-03-31 19:35:59 -0500

    feat: Header dashboard

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,15 +1,19 @@
 <x-app-layout>
-    <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            {{ __('Dashboard') }}
-        </h2>
+    <x-slot name="header">👋 Hello, {{ Auth::user()->name }}!
     </x-slot>
 
-    <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
-                <x-welcome />
-            </div>
-        </div>
-    </div>
+<div>
+
+
+{{--
+<x-mary-stat
+    title="Highlights"
+    description="This month"
+    value="22.124"
+    icon="o-arrow-trending-down"
+    class="text-orange-500"
+    color="text-pink-500"
+    tooltip-right="Gosh!" />
+</div> --}}
+
 </x-app-layout>
