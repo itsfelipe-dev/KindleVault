de8dd33abef90e045167bb37f819f571e1456d34
Andres <aforjuela11@misena.edu.co>
2025-04-02 16:55:38 -0500

    fix: place holder img

--- resources/views/components/mary-ui/card-pd.blade.php
+++ resources/views/components/mary-ui/card-pd.blade.php
@@ -5,7 +5,7 @@
     class="flex items-center max-w-lg gap-4 p-4 rounded-lg shadow-lg bg-base-200">
     <div class="flex-shrink-0 w-20 h-28">
         <img class="object-cover w-full h-full rounded-lg"
-            src="https://store.crescentbook.com.my/wp-content/uploads/2024/07/WhatsApp-Image-2024-06-29-at-12.14.57_acbd033a.jpg"
+             src="{{ $book->cover_url ?? 'https://placehold.co/620x1080?text=No+Cover' }}"
             alt="Book Cover">
     </div>
 
